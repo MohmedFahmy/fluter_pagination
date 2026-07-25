848a9b96b356d2fbcd1e9dafde480efd07423b05
MohmedFahmy <mohammedfahmy130@gmail.com>
2026-07-25 17:04:32 +0300

    Implement scroll functionality in HomeScreen with ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,8 +8,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    scrollController.addListener(_onScroll);
+    super.initState();
+  }
+
+  void _onScroll() {
+    final maxScroll = scrollController.position.maxScrollExtent;
+    final currentScroll = scrollController.position.pixels;
+    print('Max Scroll: $maxScroll, Current Scroll: $currentScroll');
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pagination')),
+      body: Center(
+        child: ListView.builder(
+          controller: scrollController,
+          itemCount: 100, // Example item count
+          itemBuilder: (context, index) {
+            return ListTile(title: Text('Item $index'));
+          },
+        ),
+      ),
+    );
   }
 }
